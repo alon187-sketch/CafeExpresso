e2c6012e8df9efd97c672ce8450ab8f4eabd5263
alon187 <alon187@gmail.com>
2026-04-30 00:54:25 +0000

    Criação e execução do test pedidos

---
+++ src/test/java/com/example/demo/PedidoTest.java
@@ -0,0 +1,92 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PedidoTest {    
+    
+    @Test
+    void deveIniciarComStatusPendente() {
+        Pedido pedido = new Pedido();
+        assertEquals(StatusPedido.PENDENTE, pedido.getStatus());
+    }
+
+    @Test
+    void deveAdicionarItensECalcularTotal() {
+
+        Produto cafe = new Produto("Café", 5.0);
+        Produto pao = new Produto("Pão", 3.0);
+
+        Pedido pedido = new Pedido();
+
+        pedido.adicionarItem(new ItemPedido(cafe, 2)); // 10
+        pedido.adicionarItem(new ItemPedido(pao, 3));  // 9
+
+        assertEquals(19.0, pedido.calcularTotal());
+    }
+
+    @Test
+    void deveMudarStatusParaPago() {
+
+        Pedido pedido = new Pedido();
+
+        pedido.pagarPedido();
+
+        assertEquals(StatusPedido.PAGO, pedido.getStatus());
+    }
+
+    @Test
+    void deveMudarStatusParaEmPreparo() {
+
+        Pedido pedido = new Pedido();
+
+        pedido.pagarPedido();
+        pedido.iniciarPreparo();
+
+        assertEquals(StatusPedido.EM_PREPARO, pedido.getStatus());
+    }
+
+    @Test
+    void deveFinalizarPedido() {
+
+        Pedido pedido = new Pedido();
+
+        pedido.pagarPedido();
+        pedido.iniciarPreparo();
+        pedido.finalizarPedido();
+
+        assertEquals(StatusPedido.FINALIZADO, pedido.getStatus());
+    }
+
+    @Test
+    void naoDeveIrParaPreparoSemPagamento() {
+
+        Pedido pedido = new Pedido();
+
+        pedido.iniciarPreparo();
+
+        assertEquals(StatusPedido.PENDENTE, pedido.getStatus());
+    }
+
+    @Test
+    void naoDeveFinalizarSemEstarEmPreparo() {
+
+        Pedido pedido = new Pedido();
+
+        pedido.finalizarPedido();
+
+        assertEquals(StatusPedido.PENDENTE, pedido.getStatus());
+    }
+
+    @Test
+    void naoDevePagarDuasVezes() {
+
+        Pedido pedido = new Pedido();
+
+        pedido.pagarPedido();
+        pedido.pagarPedido(); // tentativa inválida
+
+        assertEquals(StatusPedido.PAGO, pedido.getStatus());
+    }
+
+}
